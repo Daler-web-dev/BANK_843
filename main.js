33a1a0d4507fec6053673f41fcdf26e2fc966581
Begzod <dusmamatov46@gmail.com>
2023-03-20 14:25:04 +0500

    header integrated

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+import {header} from '/modules/header'
 let user = JSON.parse(localStorage.getItem('user'))
 
 if(!user) {
     location.assign('/pages/login.html')
 }
-
+header()
 let h1 = document.querySelector('h1')
 
 h1.innerHTML = `Welcome ${user.name} ${user.surname}`
