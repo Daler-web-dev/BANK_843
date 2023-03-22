d9bf0023e947ec9a5ec123e38f153006ca0c1107
Begzod <dusmamatov46@gmail.com>
2023-03-22 13:39:35 +0500

    fixed some issues

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import { header } from '/modules/header'
+import {colors} from '/modules/db'
+
 let user = JSON.parse(localStorage.getItem('user'))
 
 if (!user) {
@@ -8,54 +10,6 @@ header()
 let h1 = document.querySelector('h1')
 
 h1.innerHTML = `Welcome ${user.name} ${user.surname}`
-let body = document.body
-
-function reload (arr) {
-    for(let item of arr) {
-        let div = document.createElement('div')
-        let h1 = document.createElement('h1')
-        let money = document.createElement('h2')
-
-        div.classList.add('card')
-
-        h1.innerHTML = 'Visa'
-        money.innerHTML = 'RUB'
-
-        div.append(h1 , money)
-        body.append(div)
-    }
-}
-
-reload(user)
-
-console.log(user);
-
-colors = [
-    {
-        id: 1,
-        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
-    },
-    {
-        id: 2,
-        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
-    },
-    {
-        id: 1,
-        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
-    },
-    {
-        id: 2,
-        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
-    },
-    {
-        id: 1,
-        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
-    },
-    {
-        id: 2,
-        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
-    }
-]
 
 function reload(arr) {
     for (let item of arr) {
@@ -80,5 +34,3 @@ function reload(arr) {
 }
 
 reload(colors)
-
-// function creatTr () {}
