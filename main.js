a7e5313a3caa06c0a70bca54681e9cf045f4000e
Kamron <bahtierhasanzoda4@gmail.com>
2023-03-22 10:36:31 +0500

    card

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import {header} from '/modules/header'
+import { header } from '/modules/header'
 let user = JSON.parse(localStorage.getItem('user'))
 
-if(!user) {
+if (!user) {
     location.assign('/pages/login.html')
 }
 header()
@@ -9,12 +9,55 @@ let h1 = document.querySelector('h1')
 
 h1.innerHTML = `Welcome ${user.name} ${user.surname}`
 
+colors = [
+    {
+        id: 1,
+        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
+    },
+    {
+        id: 2,
+        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
+    },
+    {
+        id: 1,
+        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
+    },
+    {
+        id: 2,
+        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
+    },
+    {
+        id: 1,
+        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
+    },
+    {
+        id: 2,
+        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
+    }
+]
 
+function reload(arr) {
+    for (let item of arr) {
+        let i = document.querySelector('.i')
+        let cont = document.createElement('div')
+        let div = document.createElement('div')
+        let h1 = document.createElement('h1')
+        let h3 = document.createElement('h3')
 
-let transactions = document.querySelector('.transactions')
+        div.classList.add('card')
+        cont.classList.add('container')
 
-function creatTr () {
-    let table = document.createElement('table')
-    let tr = document
+        div.style.background = item.color
+
+        h1.innerHTML = 'Visa'
+        h3.innerHTML = 'RUB'
+
+        div.append(h1, h3)
+        cont.append(div)
+        i.append(cont)
+    }
 }
 
+reload(colors)
+
+// function creatTr () {}
