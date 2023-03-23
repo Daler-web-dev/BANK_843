35cab729feceab5d5e395f4c74f046bd4fcabee1
Kamron <bahtierhasanzoda4@gmail.com>
2023-03-23 15:46:33 +0500

    addwalet

--- modules/db.js
+++ modules/db.js
@@ -1,26 +1,20 @@
 export let colors = [
     {
         id: 1,
-        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
     },
     {
         id: 2,
-        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
     },
     {
-        id: 1,
-        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
+        id: 3,
     },
     {
-        id: 2,
-        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
+        id: 4,
     },
     {
-        id: 1,
-        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
+        id: 5,
     },
     {
-        id: 2,
-        color: ': linear - gradient(84.37deg, #D7816A 2.27 %, #BD4F6C 92.26 %)'
+        id: 6,
     }
 ]
